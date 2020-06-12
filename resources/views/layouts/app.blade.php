5156b82bb75dc9eeac49558f4e409d5ee5c5df46
idstack <idstack@outlook.co.id>
2020-06-12 13:55:38 +0000

    install laravel livewire

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @livewireStyles
 </head>
 <body>
     <div id="app">
@@ -76,5 +77,6 @@
             @yield('content')
         </main>
     </div>
+    @livewireScripts
 </body>
 </html>
