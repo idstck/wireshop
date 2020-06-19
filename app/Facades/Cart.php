73afab0e0f80303fc8c853734d4c93e2772275bc
idstack <idstack@outlook.co.id>
2020-06-19 06:55:35 +0000

    cara membuat facade di laravel

---
+++ app/Facades/Cart.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Cart extends Facade
+{
+    public static function getFacadeAccessor()
+    {
+        return 'cart';
+    }
+}
---
+++ app/Providers/CartServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use App\Helpers\Cart;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\ServiceProvider;
+
+class CartServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        App::bind('cart', function () {
+            return new Cart();
+        });
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
